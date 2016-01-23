d3d670df1054e128fc5c0ce378d26da2162f6ae8
Joe George <joe@zeroc.com>
2016-01-22 19:13:40 -0500

    Add slice2freeze

---
+++ cpp/src/slice2freeze/Makefile.mk
@@ -0,0 +1,11 @@
+#
+# slice2freeze
+#
+
+include $(CLEAR_RULES)
+LOCAL_MODULE                    = slice2freeze
+LOCAL_PATH                      = cpp/src/slice2freeze
+LOCAL_DEPENDENT_MODULES         = Slice Ice mcpp
+LOCAL_SRCDIR                    = ice/$(LOCAL_PATH)
+LOCAL_SRCS                      = $(wildcard $(LOCAL_SRCDIR)/*.cpp)
+include $(APPLICATION_RULES)
